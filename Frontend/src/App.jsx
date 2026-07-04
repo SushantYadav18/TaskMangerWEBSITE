3c76dd2b6c6c8edced306c1f3db42e5dd249566b
SushantYadav18 <shusant.kumar2bca@gmail.com>
2026-07-04 23:37:47 +0545

    create a sidebar ui and connect server

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
+import { useSelector } from "react-redux";
 
 import TaskDetails from "./pages/TaskDetails";
 import Dashboard from "./pages/Dashboard";
@@ -7,9 +8,11 @@ import Tasks from "./pages/Tasks";
 import Users from "./pages/Users";
 import Trash from "./pages/Trash";
 import Login from "./pages/Login";
+import Sidebar from "./components/Sidebar";
 
 function Layout() {
-  const user = null;
+  const {user} = useSelector((state) => state.auth);
+
   const location = useLocation();
   const isPublicRoute = location.pathname === "/log-in";
 
@@ -21,6 +24,7 @@ function Layout() {
     <div className="w-full min-h-screen flex flex-col md:flex-row bg-[#f3f4f6]">
       <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
         {/* <Sidebar /> */}
+        <Sidebar />
       </div>
 
       <div className="flex-1 h-screen overflow-y-auto">
@@ -37,7 +41,7 @@ function App() {
     <main className="w-full min-h-screen bg-[#f3f4f6]">
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/"  element={<Navigate to="/dashboard"  />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/tasks" element={<Tasks />} />
           <Route path="/completed/:status" element={<Tasks />} />
