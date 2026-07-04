5464f845c0e30c68f8a648d9210af32f930e64a1
SushantYadav18 <shusant.kumar2bca@gmail.com>
2026-07-04 23:58:57 +0545

    fully completed sidebar ui with link every pages

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -65,9 +65,39 @@ const Sidebar = () => {
   const closeSidebar = () => {
     dispatch(setOpenSidebar(false));
 }
+  const NavLink = ({ el }) => {
+    return (
+      <Link
+        to={el.link}
+        onClick={closeSidebar}
+        className={clsx(
+          "w-full lg:w-3/4 flex gap-2 px-3 py-2 rounded-full items-center text-gray-800 text-base hover:bg-[#2564ed2d]",
+          path === el.link.split("/")[0] ? "bg-blue-700 text-neutral-100" : ""
+        )}
+      >
+        {el.icon}
+        <span className='hover:text-[#2564ed]'>{el.label}</span>
+      </Link>
+    );
+  };
+
 
 return (
-    <div>sidebar</div>
+     <div className='w-full  h-full flex flex-col gap-6 p-5'>
+      <h1 className='flex gap-1 items-center'>
+        <p className='bg-blue-600 p-2 rounded-full'>
+          <MdOutlineAddTask className='text-white text-2xl font-black' />
+        </p>
+        <span className='text-2xl font-bold text-black'>TaskMe</span>
+      </h1>
+
+  <div className='flex-1 flex flex-col gap-y-5 py-8'>
+        {sidebarLinks.map((link) => (
+          <NavLink el={link} key={link.label} />
+        ))}
+      </div>
+
+    </div>
 )
 };
 
