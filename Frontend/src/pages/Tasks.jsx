f4549f892d432a1a9b31b909ab0f9e3c918c8837
SushantYadav18 <shusant.kumar2bca@gmail.com>
2026-07-08 02:00:21 +0545

    create tasks ui

--- Frontend/src/pages/Tasks.jsx
+++ Frontend/src/pages/Tasks.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
+const TABS = [
+  { title: "Board View", icon: <MdGridVie /> },
+  { title: "List View", icon: <FaList /> },
+];
+
+const TASK_TYPE = {
+  todo: "bg-blue-600",
+  "in progress": "bg-yellow-600",
+  completed: "bg-green-600",
+};
+
+
 const Tasks = () => {
+
   return (
     <div>
         Tasks Page
